6df015ab18c43cfe553fd3c119d3ecb6f2801438
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-07 11:43:12 +0700

    using router.push to navigate dynamic link
    - router.push has 2 ways like Link
    - router.replace can't back to previous page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,12 +1,20 @@
 import { useRouter } from 'next/router'
 
-function ClientProjectsPage() {
+const ClientProjectsPage = () => {
     const router = useRouter()
 
     console.log(router.query)
 
+    const loadProjectHandler = () => {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: 'quang', clientprojectid: 'a' }
+        })
+    }
+
     return <div>
         <h1>The Projects of a Given Client</h1>
+        <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
 }
 
