c8e136cb768a84ede756fc7c27dedafab6418538
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-07 11:27:04 +0700

    useRouter
    . create folder => url = folder name
    . [id] => path = id, access by using useRouter import from react/router. router.query
    .[...slug] => dynamic path queries

---
+++ pages/clients/index.js
@@ -0,0 +1,7 @@
+function ClientsPage() {
+    return <div>
+        <h1>The Client Pages</h1>
+    </div>
+}
+
+export default ClientsPage
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio page</Link>
+          <Link href="/clients">Client page</Link>
+        </li>
+      </ul>
     </div>
   )
 }
+
+export default Home
