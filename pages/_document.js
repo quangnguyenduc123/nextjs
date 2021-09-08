0f2ee85ce8bb0d0f1a60500f6830300f76f07f6e
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-08 22:20:48 +0700

    _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <div id="overlays"></div>
+                    <Main>
+                        <NextScript></NextScript>
+                    </Main>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
