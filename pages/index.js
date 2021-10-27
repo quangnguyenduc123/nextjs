bc1916154eb9c7d6b605fe76036dd784688736ca
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-10-27 21:44:54 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello Next World Master 1</h1>
+      <h1>Hello Next World Master Notify</h1>
     </div>
   )
 }
